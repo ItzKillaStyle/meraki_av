6f67eff09f8b5ff841a87d797f660ea8e9db7225
Victor <savictorac1@gmail.com>
2026-05-07 17:06:50 -0500

    asd

--- src/av_control/av_control/control_node.py
+++ src/av_control/av_control/control_node.py
@@ -17,7 +17,7 @@ PRIO_ESTOP    = 4
 
 MODEL_ACKERMANN    = 'ackermann'
 MODEL_DIFFERENTIAL = 'differential'
-MIN_PWM = 0.3
+MIN_PWM = 0.4
 
 class PID:
     def __init__(self, kp, ki, kd, out_min, out_max):
