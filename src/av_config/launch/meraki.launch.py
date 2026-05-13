e7ce56dd287ff0851f351b43a0a197dd6ebb6451
Victor <savictorac1@gmail.com>
2026-05-12 19:04:14 -0500

    asdas

--- src/av_config/launch/meraki.launch.py
+++ src/av_config/launch/meraki.launch.py
@@ -102,7 +102,7 @@ def generate_launch_description():
         name='hc12_bridge',
         parameters=[{
             'port': '/dev/ttyHC12',
-            'baud': 19200,
+            'baud': 115200,
         }],
         output='screen',
         condition=IfCondition(use_hc12),
